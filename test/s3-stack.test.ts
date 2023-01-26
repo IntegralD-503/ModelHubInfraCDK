39653da2f8671b57c798d911f6c7ddfefcb0fe6e
IntegralD-503 <hunter.clark@utexas.edu>
2023-01-26 15:34:42 -0500

    Add jest-junit dependency and update S3 stack test

--- test/s3-stack.test.ts
+++ test/s3-stack.test.ts
@@ -1,17 +1,17 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as V0 from '../lib/v0-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import { S3Stack } from '../lib/s3-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/v0-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new V0.V0Stack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+test('S3 Stack Created', () => {
+  const app = new cdk.App();
+    // WHEN
+  const stack = new S3Stack(app, 'MyTestStack');
+    // THEN
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::S3::Bucket', {
+    
+  });
 });
